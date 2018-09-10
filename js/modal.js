4f3ce62cbc89a77faa8688760b1ee2860bf4a0b7
nullorone <35527172+nullorone@users.noreply.github.com>
2018-09-10 21:42:32 +0300

    Исправляет правки сеток страниц

---
+++ js/modal.js
@@ -0,0 +1,88 @@
+var linkFormWriteUs = document.querySelector('.contacts .link');
+var linkMap = document.querySelector('.contacts .map-link');
+var overlay = document.querySelector('.popup-overlay');
+var modalWriteUs = document.querySelector('.popup-write-us');
+var btnClose = document.querySelector('.popup-write-us .btn-close');
+var btnCloseMap = document.querySelector('.popup-map .btn-close');
+var modalMap = document.querySelector('.popup-map');
+var mapPic = document.querySelector('.popup-map .map-pic');
+var form = document.querySelector('.popup-write-us__form');
+var formLogin = document.querySelector('.popup-write-us__form #name');
+var formEmail = document.querySelector('.popup-write-us__form #email');
+var isStorageSupport = true;
+var storage = '';
+var iframe = document.querySelector('.popup-map iframe');
+var iframeStatus = '';
+
+// Проверка работы localStorage
+try {
+  storage = localStorage.getItem('userLogin');
+} catch (err) {
+  isStorageSupport = false;
+}
+// Проверка загрузки iframe с картой Яндекса
+try {
+  var iframeDocument = iframe.contentWindow.document || iframe.contentDocument;
+  if (iframeDocument.readyState  == 'complete') {
+    iframeStatus = true;}
+} catch (err) {
+    iframeStatus = false;
+}
+
+// Методы при взаимодействии с элементами
+function hideModalWriteUs(evt) {
+  evt.preventDefault();
+  overlay.classList.remove('popup-overlay--active');
+  modalWriteUs.classList.remove('popup-write-us--active');
+}
+function hideModalMap(evt) {
+  evt.preventDefault();
+  overlay.classList.remove('popup-overlay--active');
+  modalMap.classList.remove('popup-map--active');
+}
+function showModalWriteUs(evt) {
+  evt.preventDefault();
+  modalWriteUs.classList.add('popup-write-us--active');
+  overlay.classList.add('popup-overlay--active');
+  if (storage) {
+    formLogin.value = storage;
+    formEmail.focus();
+  } else {
+    formLogin.focus();
+  };
+  window.addEventListener('keydown', function(evt) {
+    if (evt.keyCode === 27) {
+      hideModalWriteUs(evt);
+    }});
+}
+function showModalMap(evt) {
+  evt.preventDefault();
+  modalMap.classList.add('popup-map--active');
+  overlay.classList.add('popup-overlay--active');
+  if (iframeStatus) {
+    mapPic.style.display = 'none';
+  }
+  window.addEventListener('keydown', function(evt) {
+    if (evt.keyCode === 27) {
+      hideModalMap(evt);
+    }});
+}
+function formValue(evt) {
+  if (!formLogin.value || !formEmail.value) {
+    evt.preventDefault();
+// Добавить анимацию на поля без значений
+  } else {
+    if (isStorageSupport) {
+    localStorage.setItem('userLogin', formLogin.value);
+    localStorage.setItem('userPassword', formEmail.value);
+  }};
+  // добавить удаление класса с ошибкой ввода поля
+}
+// Действия с использованием методов
+linkFormWriteUs.addEventListener('click', showModalWriteUs);
+linkMap.addEventListener('click', showModalMap);
+overlay.addEventListener('click', hideModalWriteUs);
+overlay.addEventListener('click', hideModalMap);
+btnClose.addEventListener('click', hideModalWriteUs);
+btnCloseMap.addEventListener('click', hideModalMap);
+form.addEventListener('submit', formValue);
